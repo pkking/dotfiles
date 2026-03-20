239a1854d8fe819cd22ff09ef7e5dc276f35e79c
lichaoran <pkwarcraft@gmail.com>
2026-03-20 12:57:43 +0800

    feat: add homebrew
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- dotfiles/dot_bashrc.d/configure.sh
+++ dotfiles/dot_bashrc.d/configure.sh
@@ -3,6 +3,8 @@ export PATH=$PATH:$HOME/.local/bin:$HOME/.nix-profile/bin
 eval "$(mise activate bash --shims)"
 # starship
 eval "$(starship init bash)"
+# homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 if command -v nvim >/dev/null 2>&1; then
     alias vi='nvim'
